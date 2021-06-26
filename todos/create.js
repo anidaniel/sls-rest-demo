513c19e5795ce04cf014bf6c2cfbe65ac03f7763
Anidaniel <theanidaniel@gmail.com>
2021-06-27 03:26:10 +0530

    changes

--- todos/create.js
+++ todos/create.js
@@ -1,47 +1,52 @@
 'use strict';
 
-const AWS = require('aws-sdk');
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.create = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  if (typeof data.text !== 'string') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create the todo item.',
+    });
+    return;
+  }
 
-    const timestamp = new Date().getTime();
-    const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: uuid.v1(),
+      text: data.text,
+      checked: false,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
+  };
 
-    if (typeof data.text !== 'string'){
-        console.error('Validation Failed');
-        callback(new Error('Couldn\'t create the todo item.'));
-        return;
+  // write the todo to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the todo item.',
+      });
+      return;
     }
 
-    const params = {
-        TableName: 'todos',
-        Item: {
-            id: uuid.v1(),
-            text: data.text,
-            checked: false,
-            createdAt: timestamp,
-            updatedAt: timestamp
-        }
-    }
-
-    dynamoDb.put(params, (error, result) =>{
-        if(error) {
-            console.log(error);
-            callback(new Error('Couldn\'t create the todo item.'));
-            return;
-        }
-
-        // const response = {
-        //     statusCode: 200,
-        //     body: JSON.stringify(results.Item)
-        // }
-        // callback(null, response);
-        callback(null, {
-            statusCode: 200,
-            body: JSON.stringify(message),
-            headers: {'Content-Type': 'application/json'}
-        });
-    })
-}
-
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+};
